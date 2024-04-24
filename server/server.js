aec20130aec3a39b1e64ba5e033ebb439eeae1f7
mackerson123 <mackreiferson99@gmail.com>
2024-04-23 17:12:49 -0700

    Revamp quickstart (#6)
    * changes
    
    * readme
    
    * remove id

--- server/server.js
+++ server/server.js
@@ -1,60 +1,64 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const session = require('express-session')
-const app = express()
-const axios = require("axios")
-require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const app = express();
+const axios = require("axios");
+require("dotenv").config();
 
 // middleware
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // session
-app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true
-}))
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
-const PARAFIN_BASE_URL = 'https://api.parafin.com/v1'
+const PARAFIN_BASE_URL = "https://api.parafin.com/v1";
+const PARAFIN_DEV_BASE_URL = "https://api.dev.parafin.com/v1";
 
 // route for fetching Parafin token
-app.get("/parafin/token/:id", async (req, res) => {
-  const url = `${PARAFIN_BASE_URL}/auth/redeem_token`
+app.get("/parafin/token/:id/:isDev?", async (req, res) => {
   const personId = req.params.id;
+  const isDev = req.params.isDev;
+  const url = `${
+    isDev === "true" ? PARAFIN_DEV_BASE_URL : PARAFIN_BASE_URL
+  }/auth/redeem_token`;
   console.log(personId);
-  let token;
-  let error;
-  
+
   const data = {
-    person_id: personId
-  }
-  
+    person_id: personId,
+  };
+
   const config = {
     auth: {
       username: process.env.PARAFIN_CLIENT_ID,
       password: process.env.PARAFIN_CLIENT_SECRET,
-    }
-  }  
-  
+    },
+  };
+
   try {
     // make call to fetch Parafin token for business
-    const result = await axios.post(url, data, config)
-    parafinToken = result.data.bearer_token;
-  
+    const result = await axios.post(url, data, config);
+    const parafinToken = result.data.bearer_token;
+
     res.send({
-      parafinToken: parafinToken
-    })
-  } catch(error) {
+      parafinToken: parafinToken,
+    });
+  } catch (error) {
     console.log(error.response.data);
     res.send({
       errorCode: error.response.status,
-      message: error.response.data
-    })
+      message: error.response.data,
+    });
   }
 });
 
 // Starting Server
 app.listen(process.env.PORT || 8080, () => {
-	console.log(`App listening on PORT: ${process.env.PORT || 8080}`)
-})
+  console.log(`App listening on PORT: ${process.env.PORT || 8080}`);
+});
--- src/App.js
+++ src/App.js
@@ -1,46 +1,109 @@
-import { useEffect, useState, useCallback } from "react";
-import axios from 'axios'
-import { ParafinElements } from "@parafin/react-parafin-elements";
-
+import { useEffect, useState } from "react";
+import axios from "axios";
+import styled from "styled-components";
+import { ParafinWidget } from "@parafin/react";
 
 function App() {
-  const [token, setToken] = useState(null)
-  const [state, setState] = useState(null)
+  const [token, setToken] = useState(null);
 
   useEffect(() => {
     const fetchToken = async () => {
-      
-      // replace with your own Person ID
-      const personId = "<your-person-id>" 
-      
-      // fetch Parafin token from server
-      const response = await axios.get(`/parafin/token/${personId}`) 
-      setToken(response.data.parafinToken)
-    }
+      // Replace with your own Person ID. It should begin with "person_".
+      const personId = "<your-person-id>";
 
-    if(!token) {
-      fetchToken()
-    }    
-  })
+      // Change to false to use production or sandbox production environment
+      const isDevEnvironment = true;
+
+      // Fetch Parafin token from server
+      const response = await axios.get(
+        `/parafin/token/${personId}/${isDevEnvironment}`
+      );
+      setToken(response.data.parafinToken);
+    };
+
+    if (!token) {
+      fetchToken();
+    }
+  });
+
+  const onOptIn = async () => ({
+    businessExternalId: "<your-external-business-id>",
+    legalBusinessName: "Hearty Kitchens LLC",
+    dbaName: "Hearty Kitchens",
+    ownerFirstName: "Ralph",
+    ownerLastName: "Furman",
+    accountManagers: [
+      {
+        name: "Vineet Goel",
+        email: "test1@parafin.com",
+      },
+    ],
+    routingNumber: "121141822",
+    accountNumberLastFour: "6789",
+    bankAccountCurrencyCode: "USD",
+    email: "test2@parafin.com",
+    phoneNumber: "2026331000",
+    address: {
+      addressLine1: "301 Howard St",
+      city: "San Francisco",
+      state: "CA",
+      postalCode: "94105",
+      country: "USA",
+    },
+  });
 
   return (
-    <div style={{width: "600px"}}>
-      <h1>Parafin Elements Quickstart</h1>
+    <div>
+      <Header>
+        <h1>Parafin Widget Quickstart</h1>
+      </Header>
       {token ? (
-        <div>
-          <ParafinElements
-            product="capital"            
-            environment="production"
+        <WidgetContainer>
+          <ParafinWidget
             token={token}
-          />              
-        </div> 
+            product="capital"
+            // Optional props below, see docs.parafin.com for more information
+            externalBusinessId={undefined}
+            onOptIn={onOptIn}
+            // For demo purposes only. Do not use in your integration.
+            widgetUrlOverride="https://embedded-demo-widget.vercel.app"
+            dashboardUrlOverride="https://embedded-demo-app.vercel.app"
+          />
+          <ParafinWidget
+            token={token}
+            product="banking"
+            // Optional props below, see docs.parafin.com for more information
+            externalBusinessId={undefined}
+            onOptIn={onOptIn}
+            // For demo purposes only. Do not use in your integration.
+            widgetUrlOverride="https://embedded-demo-widget.vercel.app"
+            dashboardUrlOverride="https://embedded-demo-app.vercel.app"
+          />
+        </WidgetContainer>
       ) : (
-        <div>
-          loading...
-        </div>         
+        <Shell>loading...</Shell>
       )}
     </div>
   );
 }
 
 export default App;
+
+const Header = styled.div`
+  height: 120px;
+  background-color: white;
+  display: flex;
+  align-items: center;
+  padding: 20px;
+`;
+
+const Shell = styled.div`
+  padding: 20px;
+`;
+
+const WidgetContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+  gap: 40px;
+`;
